aeb0a2e569ddb55dba79848a9415de0d72fe8ddb
sergeidanilov <sergeidanilov@gmail.com>
2023-11-14 23:12:54 -0800

    Update installation script to download latest release from github and updated readme

--- README.md
+++ README.md
@@ -22,3 +22,14 @@ My use case is to typically press hotkey to lock the screen which triggers turni
 I also have automatic screen lock in KDE setup , which also triggers screen turn off in case i forgot to lock.
 
 # Installation
+Run the install.sh that will install [kde-turn-off-screen](https://github.com/danilovsergei/kde-turn-off-screen/releases/latest/download/kde-turn-off-screen) into "$HOME/bin" or provide your own bin dir.
+
+```
+bash -c "$(curl -L https://raw.githubusercontent.com/danilovsergei/kde-turn-off-screen/main/install.sh)" -- "$HOME/bin"
+```
+
+Or manually:
+* download latest [kde-turn-off-screen](https://github.com/danilovsergei/kde-turn-off-screen/releases/latest/download/kde-turn-off-screen) binary and save it to your scripts dir\
+* download systemd user service [kde-turn-off-screen.service](https://github.com/danilovsergei/kde-turn-off-screen/releases/latest/download/kde-turn-off-screen.service) and edit ExecStart to provide full kde-turn-off-screen path
+* start kde-turn-off-screen.service as user
+
--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-script_dir=$(dirname `realpath "$0"`)
-cd $script_dir
 bin_name="kde-turn-off-screen"
 abs_bin_path=$bin_dir/$bin_name
 service_file=kde-turn-off-screen.service
 systemd_dir=$HOME/.config/systemd/user
+latest_release="https://github.com/danilovsergei/kde-turn-off-screen/releases/latest/download/"
 
 # creates directory if it does not exist
 function create_dir() {
@@ -21,28 +20,35 @@ fi
 
 # location kde-turn-off-screen binary will be installed
 bin_dir="$1"
-
 create_dir $bin_dir
+
+echo -e "Download latest release from github $latest_release\n"
+temp_dir=$(mktemp -d)
+cd "$temp_dir"
+curl -s -L "$latest_release/$bin_name" -o "$bin_name"
+curl -s -L "$latest_release/$service_file" -o "$service_file"
+chmod +x kde-turn-off-screen
+
 
 echo -e "Copy $bin_name into $bin_dir\n"
 cp -f $bin_name "$bin_dir/"
 
-echo "Replace <bin_dir> with $bin_dir in $service_file"  
+echo -e "Replace <bin_dir> with $bin_dir in $service_file\n"  
 sed -i "s|=<bin_dir>|=$bin_dir|g" $service_file
 
 if ! ( grep -q "$abs_bin_path" "$service_file" ); then
   echo "Failed to replace ExecStart in $service_file to $abs_bin_path"
 fi
 
-echo "Remove TODO line from $service_file"
+echo -e "Remove TODO line from $service_file \n"
 sed -i '/^# TODO/d' "$service_file"
 
 
-echo "Copy $service_file to $systemd_dir"
+echo -e "Copy $service_file to $systemd_dir\n"
 create_dir $systemd_dir
 cp -f $service_file "$systemd_dir/"
 
-echo "Start $service_file service"
+echo -e "Start $service_file service\n"
 systemctl --user enable $service_file
 systemctl --user start $service_file
 
@@ -52,4 +58,4 @@ if ! ( echo $service_status | grep -q "active" ); then
   echo "Run systemctl --user status $service_file for details"
 fi
 
-echo "Service $service_file started"
+echo -e "Service $service_file started\n"
